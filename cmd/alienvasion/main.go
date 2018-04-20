8aab9b591b1bb29f82b3cd43df805630a0854645
Alessio Treglia <alessio@debian.org>
2018-04-20 22:29:13 +0100

    Improve -help screen

--- cmd/alienvasion/main.go
+++ cmd/alienvasion/main.go
@@ -27,6 +27,34 @@ var (
 )
 
 func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Mad aliens are about to invade the earth and this
+program aims to simulate the invasion.
+
+Other than the flags described below, this program reads a map from
+STDIN. A map contains the names of cities in the non-existent world of
+X, one city each line. The city name comes first, followed by 1-4
+directions (north, south, east, or west). Each one represents a
+road to another city that lies in that direction.
+The city and each of the pairs are separated by a single space, and
+the directions are separated from their respective cities with an
+equals (=) sign.
+
+N aliens are created and given random name, where N is specified as
+an optional command-line argument. These aliens start out at random
+places on the map, and wander around randomly, following links. Each
+iteration, the aliens can travel in any of the directions leading out
+of a city.
+
+When multiple aliens end up in the same place, they fight, and in the
+process kill each other and destroy the city. When a city is destroyed,
+it is removed from the map, and so are any roads that lead into or out
+of it. Once a city is destroyed, aliens can no longer travel to or
+through it. This may lead to aliens getting "trapped".
+
+`)
+		flag.PrintDefaults()
+	}
 	flag.IntVar(&naliens, "naliens", DefaultNAliens, "generate N aliens")
 	flag.IntVar(&maxmoves, "maxmoves", DefaultMaxMoves, "max number of moves each alien can make")
 	log.SetPrefix(fmt.Sprintf("%s: ", path.Base(os.Args[0])))
